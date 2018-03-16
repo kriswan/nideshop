df7c85e648a6bc1feec28f260d2802ee0fcf2205
shunfei.wan <shunfei.wan@qunar.com>
2018-03-16 18:02:56 +0800

    修改微信appid 和 数据库

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: '', // 小程序 appid
+    appid: 'wx33573eb1cf77035e', // 小程序 appid
     secret: '', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
--- src/common/config/database.js
+++ src/common/config/database.js
@@ -8,6 +8,6 @@ module.exports = {
   host: '127.0.0.1',
   port: '3306',
   user: 'root',
-  password: 'root',
+  password: 'LzEY2FNR7Z6QRC!',
   dateStrings: true
 };
